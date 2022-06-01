41631a72969844093daeba732d37ec6b1590ae11
David Ortiz <ortizcdavid@gmail.com>
2022-06-01 14:15:58 -0700

    arguments electron

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -18,12 +18,13 @@ module.exports = (on, config) => {
       return arguments_;
     }
 
-    // if (browser.name === 'electron') {
-    //   arguments_['width'] = 1920;
-    //   arguments_['height'] = 1080;
-    //   arguments_['resizable'] = false;
-    //   return arguments_;
-    // }
+    if (browser.name === 'electron') {
+      arguments_.args.push(
+        '--disable-background-timer-throttling',
+        '--disable-backgrounding-occluded-windows',
+        '--disable-renderer-backgrounding',
+      );
+    }
 
     // metamask welcome screen blocks cypress from loading
     if (browser.name === 'chrome') {
